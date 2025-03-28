f3d05f9fc5b141efba57b9021e456734c527828e
Jason <jason.matillano@gmail.com>
2025-03-28 08:31:28 +0800

    003VuetifyTeambleApp

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+
+    <Body></Body>
+
+</template>
+
+<script>
+import Body from "./Body.vue"
+export default {
+    name: 'App',
+    components: {
+        Body
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
