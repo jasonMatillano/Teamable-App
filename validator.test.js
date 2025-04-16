40bf7700da279fb36387bd4282cc618b94b467fe
Jason <jason.matillano@gmail.com>
2025-04-16 08:07:46 -0400

    006CreatedUnitTest

---
+++ validator.test.js
@@ -0,0 +1,41 @@
+const {  isInvalidEmail, isEmptyPayload } = require('./validator');
+
+test ('invalid email', () => {
+    const testPayload = {
+        name: 'name', 
+        email: 'email', 
+        interests: 'interests'
+    };
+    
+    const result = isInvalidEmail(testPayload);
+    expect(result).toBe(true);
+})
+
+test ('valid email', () => {
+    const testPayload = {
+        name: 'name', 
+        email: 'email@gmail.com', 
+        interests: 'interests'
+    };
+    
+    const result = isInvalidEmail(testPayload);
+    expect(result).toBe(false);
+})
+
+test ('empty payload', () => {
+    const testPayload = {};
+    
+    const result = isEmptyPayload(testPayload);
+    expect(result).toBe(true);
+})
+
+test ('valid payload', () => {
+    const testPayload = {
+        name: 'name', 
+        email: 'email@gmail.com', 
+        interests: 'interests'
+    };
+    
+    const result = isEmptyPayload(testPayload);
+    expect(result).toBe(false);
+})
